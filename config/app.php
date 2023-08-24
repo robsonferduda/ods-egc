2d26d57e928c3b21680b0d9a12b59ed1a2861944
Robson <robson@coperve.ufsc.br>
2023-08-24 10:20:58 -0300

    Composer Update

--- config/app.php
+++ config/app.php
@@ -161,8 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,9 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-
     ],
 
 ];
