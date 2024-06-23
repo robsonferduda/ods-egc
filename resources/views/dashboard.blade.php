4ac850dfcdbad43ac2a2c037fa8151db5f0c22b6
Robson <robson@coperve.ufsc.br>
2024-06-23 15:37:21 -0300

    Atualização Chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -497,7 +497,7 @@
 
                     var ctx = document.getElementById("myChart").getContext('2d');
                     var myChart = new Chart(ctx, {
-                        type: 'bar',
+                        type: 'line',
                         data: {
                             labels: data.ods,
                             datasets: [{
