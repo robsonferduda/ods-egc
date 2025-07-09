d0c6bf3fab4a200da85835bfba0345a303bb3833
Robson Fernando Duda <robsonferduda@gmail.com>
2025-07-08 21:22:36 -0300

    Grafo

--- resources/views/grafo.blade.php
+++ resources/views/grafo.blade.php
@@ -45,7 +45,7 @@
 
           const options = {
             nodes: { shape: 'dot', size: 10, font: { size: 14 } },
-            edges: { arrows: 'none', smooth: true },
+            edges: { arrows: 'to', smooth: true },
             physics: { stabilization: false }
           };
 
