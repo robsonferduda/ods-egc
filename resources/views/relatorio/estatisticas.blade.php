af2b991119097c2fccca4768b3229daef6bee607
Robson Fernando Duda <robsonferduda@gmail.com>
2025-10-09 20:47:10 -0300

    Dashboard

--- resources/views/relatorio/estatisticas.blade.php
+++ resources/views/relatorio/estatisticas.blade.php
@@ -1,4 +1,10 @@
 <h3 class="text-align: center;">Perfil ODS - Relatório de Diagnóstico</h3>
 <h4><strong>Centro</strong>:</h4>
-<img src="{{ $grafico_total }}" style="width: 100px;" />
-<img src="{{ $grafico_evolucao }}" style="width: 100px;" />
+<div>
+	<div style="float: left;">
+		<img src="{{ $grafico_evolucao }}" style="width: 100%;" />
+	</div>
+	<div style="float: rigth;">
+		<img src="{{ $grafico_total }}" style="width: 100%;" />
+	</div>
+</div>
