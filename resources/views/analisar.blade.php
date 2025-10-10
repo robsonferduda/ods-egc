debe4abb4d7bda55f7e40774ab40d5796c9f9d9e
Robson Fernando Duda <robsonferduda@gmail.com>
2025-10-10 16:52:56 -0300

    Atualização de Nome

--- resources/views/analisar.blade.php
+++ resources/views/analisar.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-12">
        <div class="header-text">
          <div class="header-text">
-            <h3 class="mb-0">
+            <h3>
                 {{ config('app.name') }} <i class="fa fa-angle-double-right" aria-hidden="true"></i> Analisar Documentos
                 @include('layouts.nivel')
             </h3>
