3a034e80900e8637cbb9649b0852e87a76f4b332
Robson Fernando Duda <robsonferduda@gmail.com>
2025-10-09 22:12:08 -0300

    Dashboard

--- resources/views/relatorio/estatisticas.blade.php
+++ resources/views/relatorio/estatisticas.blade.php
@@ -44,7 +44,61 @@
 	<h5 style="margin-bottom: 3px; margin-top: 3px; padding-top: 0px;">Índice de Crescimento Sustentável: {{ $indice_crescimento_sustentavel }}</h5>
 	<h5 style="margin-bottom: 3px; margin-top: 3px; padding-top: 0px;">Índice de Engajamento Sustentável: {{ $indice_engajamento_sustentavel }}</h5>
 	<h5 style="margin-bottom: 3px; margin-top: 3px; padding-top: 0px;">Docente Destaque: {{ $docente_destaque }}</h5>
-		
+	
+	<div class="row">
+                    <!--<div class="col-md-4" id="card-dimensao-centro"></div>-->
+                    <div class="col-md-4" id="card-dimensao-ods">
+                                <div class="card shadow-sm mb-2" style="background: #f3f3f3;">
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">Institucional</h5>
+                                        <p class="card-text mb-1">
+                                            <span class="display-4 font-weight-bold">35.3%</span>
+                                        </p>
+                                        <small class="text-muted">Dimensão ODS mais destacada no CFH</small>
+                                    </div>
+                                </div>
+                            </div>
+                    <div class="col-md-4" id="card-ics">
+                                <div class="card shadow-sm mb-2" style="background: #f3f3f3;">
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">
+                                            ICS
+                                            <span class="pull-right badge badge-pill badge-danger" style="font-size: 1rem; vertical-align: top; margin-left: 8px;">Queda</span>
+                                        </h5>
+                                        <p class="card-text mb-1">
+                                            <span class="display-4 font-weight-bold">23.3</span>                                            
+                                        </p>
+                                        <small class="text-muted">Índice de Crescimento Sustentável</small>
+                                    </div>
+                                </div>
+                            </div>
+                    <div class="col-md-4" id="card-ies">
+                                <div class="card shadow-sm mb-2" style="background: #f3f3f3;">
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">
+                                            IES
+                                            <span class="pull-right badge badge-pill badge-warning" style="font-size: 1rem; vertical-align: top; margin-left: 8px;">Médio</span>
+                                        </h5>
+                                        <p class="card-text mb-1">
+                                            <span class="display-4 font-weight-bold">60.7</span>                                           
+                                        </p>
+                                        <small class="text-muted">Índice de Engajamento Sustentável</small>
+                                    </div>
+                                </div>
+                            </div>
+                    <div class="col-md-12" id="card-pesquisador-centro">
+                                <div class="card shadow-sm mb-2 mt-3" style="background: #f3f3f3;">
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">ANDRÉIA ISABEL GIACOMOZZI</h5>
+                                        <p class="card-text mb-1">
+                                            <span class="display-4 font-weight-bold">22</span>
+                                        </p>
+                                        <small class="text-muted">Pesquisador(a) com mais documentos no centro CFH</small>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+				
 	<h4 style="margin-top: 10px"><strong>Gráficos de Desempenho</strong>:</h4>
     <table>
         <tr>
