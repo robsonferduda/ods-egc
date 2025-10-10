8e63a5379a8d60da61762ebd1d2971d6e1a20c6d
Robson Fernando Duda <robsonferduda@gmail.com>
2025-10-09 21:35:27 -0300

    Dashboard

--- resources/views/relatorio/estatisticas.blade.php
+++ resources/views/relatorio/estatisticas.blade.php
@@ -44,64 +44,7 @@
 	<h5 style="margin-bottom: 3px; margin-top: 3px; padding-top: 0px;">Índice de Crescimento Sustentável: {{ $indice_crescimento_sustentavel }}</h5>
 	<h5 style="margin-bottom: 3px; margin-top: 3px; padding-top: 0px;">Índice de Engajamento Sustentável: {{ $indice_engajamento_sustentavel }}</h5>
 	<h5 style="margin-bottom: 3px; margin-top: 3px; padding-top: 0px;">Docente Destaque: {{ $docente_destaque }}</h5>
-	
-	<table>
-		<thead>
-		<tr>
-			<td colspan="18" style="text-align: center;"><strong>Relatório de Evolução por ODS/Ano</strong></td>
-		</tr>
-		<tr>
-			<th style="font-weight: bold; text-align: center;">Ano</th>
-			<th style="font-weight: bold; text-align: center;">ODS 1</th>
-			<th style="font-weight: bold; text-align: center;">ODS 2</th>
-			<th style="font-weight: bold; text-align: center;">ODS 3</th>
-			<th style="font-weight: bold; text-align: center;">ODS 4</th>
-			<th style="font-weight: bold; text-align: center;">ODS 5</th>
-			<th style="font-weight: bold; text-align: center;">ODS 6</th>
-			<th style="font-weight: bold; text-align: center;">ODS 7</th>
-			<th style="font-weight: bold; text-align: center;">ODS 8</th>
-			<th style="font-weight: bold; text-align: center;">ODS 9</th>
-			<th style="font-weight: bold; text-align: center;">ODS 10</th>
-			<th style="font-weight: bold; text-align: center;">ODS 11</th>
-			<th style="font-weight: bold; text-align: center;">ODS 12</th>
-			<th style="font-weight: bold; text-align: center;">ODS 13</th>
-			<th style="font-weight: bold; text-align: center;">ODS 14</th>
-			<th style="font-weight: bold; text-align: center;">ODS 15</th>
-			<th style="font-weight: bold; text-align: center;">ODS 16</th>
-			<th style="font-weight: bold; text-align: center;">Total Geral</th>
-		</tr>
-		</thead>
-		<tbody>
-			@foreach($lista as $key => $d)
-				@if($key < (count($lista)))
-				@php
-					$formatacao = ($key > (count($lista) - 3)) ? 'font-weight: bold;' : '';
-				@endphp
-					<tr>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['ano'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['1'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['2'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['3'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['4'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['5'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['6'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['7'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['8'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['9'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['10'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['11'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['12'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['13'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['14'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['15'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['16'] }}</td>
-						<td style="{{ $formatacao }} text-align: center;">{{ $d['total'] }}</td>
-					</tr>
-				@endif
-			@endforeach
-		</tbody>
-	</table>
-	
+		
 	<h4 style="margin-top: 10px"><strong>Gráficos de Desempenho</strong>:</h4>
     <table>
         <tr>
