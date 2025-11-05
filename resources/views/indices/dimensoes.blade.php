9eb625a6a0b8bdb017122c6a1d3ec390c449bcee
Robson <robson@coperve.ufsc.br>
2025-11-05 09:46:42 -0300

    Atualização Dashboard

---
+++ resources/views/indices/dimensoes.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.guest')
+@section('content')
+<div class="row">
+    <div class="row mb-5 mt-3">
+        <div class="col-md-2 col-sm-2">
+            <img src="{{ asset('img/logo-egc.png') }}" class="img-fluid mt-1" alt="Responsive image">
+        </div>
+        <div class="col-md-10 col-sm-10">
+            <h6 class="mb-0">Universidade Federal de Santa catarina (UFSC)</h6>
+            <h6 class="mb-0 mt-1">Programa de Pós-graduação em Engenharia, Gestão e Mídia do Conhecimento (PPGEGC)</h6>
+            <h6 class="mb-0 mt-1">Engenharia do Conhecimento/Teoria e prática em Engenharia do Conhecimento</h6>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12">
+            <p class="mb-1"><strong>Dimensões ODS</strong></p>    
+        </div>
+    </div>  
+</div>
+@endsection
